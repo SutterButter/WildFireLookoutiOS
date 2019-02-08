073297d5d8ed16a179ab142a7db0ee4c9c58af56
SutterButter <aeontruths@gmail.com>
2019-02-08 00:11:18 -0500

    Initial Commit

---
+++ Wildfire Lookout/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Wildfire Lookout
+//
+//  Created by Noah Sutter on 2/8/19.
+//  Copyright © 2019 Noah Sutter. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Wildfire Lookout/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Wildfire Lookout
+//
+//  Created by Noah Sutter on 2/8/19.
+//  Copyright © 2019 Noah Sutter. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
